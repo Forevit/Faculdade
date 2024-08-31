eb5435e23ec6f2ae50bd81be00d8311710203828
Eduardo Ferreira <c.eduardo.r.ferreira@gmail.com>
2024-08-30 21:30:48 -0300

    aula de python (programação de computadores)

---
+++ src/Aulas da faculdade/Hello World.py
@@ -0,0 +1,14 @@
+nome_aluno = input ("Digite o nome do aluno:")
+curso = input ("Digite o curso do aluno:")
+instituicao = input ("digite o nome da instituição: ")
+professor = input ("Digite o nome do Professor: ")
+semestre = input ("Digite em que semestre você está: ")
+
+#nome_aluno = ''
+#curso = ''
+#Instituicao = ''
+#Professor = ''
+#Semestre = '0.0'
+
+apresentacao = f"Boa tarde! Meu nome é {nome_aluno} sou Estudante do curso de {curso} na UniAteneu ministrada pelo professor {professor} no semestre {semestre}"
+print(apresentacao)
